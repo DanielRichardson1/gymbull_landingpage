644209387aea1088ba152f4bfc2a164ccc875936
danielrichardson1 <drich1475@gmail.com>
2025-03-02 00:13:34 -0500

    added gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
 import Pricing from "@/components/Pricing/Pricing";
 import FAQ from "@/components/FAQ";
-import Logos from "@/components/Logos";
 import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
 import Section from "@/components/Section";
@@ -13,7 +12,9 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Hero />
-      <Logos />
+
+      <div className="w-full py-16 bg-gradient-to-b from-[rgba(202,208,230,0.5)] to-transparent"></div>
+
       <Container>
         <Benefits />
 
